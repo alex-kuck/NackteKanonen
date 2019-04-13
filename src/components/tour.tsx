a4c997aa49bfd4fdd8af39827718d44d1b119fc4
Alex Kucksdorf <alexander.kucksdorf@gmail.com>
2019-04-13 15:36:32 +0200

    Add additional tour hints

--- src/components/tour.tsx
+++ src/components/tour.tsx
@@ -1,49 +1,51 @@
 import React from "react";
-import { useCountdown } from "../utils";
-import { Link } from "react-router-dom";
-import { Bold, TextCentered } from "../styled_components";
+import {useCountdown} from "../utils";
+import {Link} from "react-router-dom";
+import {Bold, TextCentered} from "../styled_components";
 
 export const TourLink: React.FC = () => {
     const countDown = useCountdown(Date.UTC(2019, 4, 1, 13, 0, 0));
 
     return (
         <Link to={'/tour'}>
-            NK Tour '19<br />
-            <span style={{ fontSize: '.8rem' }}>{countDown}</span>
+            NK Tour '19<br/>
+            <span style={{fontSize: '.8rem'}}>{countDown}</span>
         </Link>
     );
 };
 
 export const Tour: React.FC = () => {
-    const secondHint = Date.UTC(2019, 3, 10, 19, 0, 0);
-    const countDown = useCountdown(secondHint);
+    const nextHint = Date.UTC(2019, 3, 20, 18, 0, 0);
+    const countDown = useCountdown(nextHint);
     return (
         <>
             <h1>Tour '19</h1>
             <h2>Nächste Informationen in:</h2>
-            <TextCentered><h2 style={{ fontSize: '2rem' }}>{countDown}</h2></TextCentered>
+            <TextCentered><h2 style={{fontSize: '2rem'}}>{countDown}</h2></TextCentered>
             <ol className={'tourHints'}>
                 <li>
                     In euren Umschlägen habt ihr einen ersten Hinweis auf das Programm der
                     diesjährigen Kegeltour bekommen.
-                    Dort werdet ihr alle in <Bold>neue Rollen</Bold> schlüpfen - und wie ginge das besser,
+                    Dort werdet ihr alle in <Bold>neue Rollen</Bold> schlüpfen - und wie ginge das
+                    besser,
                     als mit den entsprechenden <Bold>Outfits</Bold>?
                     Fangt also mit euren Besorgungen an, damit ihr nicht am Ende mit
                     heruntergelassener oder sogar ohne Hose dasteht.
-                    <br />
-                    Außerdem muss jeder von euch <Bold>seinen Teil</Bold> dazu beitragen, damit ihr das
+                    <br/>
+                    Außerdem muss jeder von euch <Bold>seinen Teil</Bold> dazu beitragen, damit ihr
+                    das
                     diesjährige Reiseziel erreicht. Wenn ihr - und eure Navis -
                     nicht nur <Bold>???</Bold> sehen wollt, ist es wichtig, dass ihr euren Teil des
                     Rätsels gelöst zur Abreise mitbringt.
-                    <br /><br />
+                    <br/><br/>
                     <span style={{
                         fontStyle: 'italic',
                         fontSize: '.8rem'
                     }}><Bold>Extra-Tipp:</Bold>&nbsp;
-                                Benutzt zu Hause etwas Kleber, wenn ihr uns nicht auf den Leim gehen und schon am Start kleben bleiben wollt.
+                        Benutzt zu Hause etwas Kleber, wenn ihr uns nicht auf den Leim gehen und schon am Start kleben bleiben wollt.
                     </span>
                 </li>
-                {secondHint < Date.now() && <li>
+                <li>
                     <span>Ich packe meinen Koffer...</span>
                     <ul>
                         <li>ausreichend Klamotten für warmes und kaltes Wetter</li>
@@ -53,19 +55,36 @@ export const Tour: React.FC = () => {
                         <li>Badetuch</li>
                         <li>Kleidung für deine Rolle (siehe Umschlag)</li>
                         <li>dein Puzzle (fertig mitbringen zum Treffpunkt)</li>
-                        <li>Taschengeld ( wir versuchen wie bei vorherigen Touren das meiste zu organisieren, jedoch kann es sein, dass ihr z. B. in Bars, Kasinos oder auf dem Strip selber zahlen müsst)</li>
+                        <li>Taschengeld ( wir versuchen wie bei vorherigen Touren das meiste zu
+                            organisieren, jedoch kann es sein, dass ihr z. B. in Bars, Kasinos oder
+                            auf dem Strip selber zahlen müsst)
+                        </li>
                         <li>Personalausweis/ Reisepass</li>
                         <li>Krankenversicherungskarte</li>
                         <li>EC- und/ oder Kreditkarte</li>
-                        <li>eine kleine Reiseapotheke (z.B. Sonnencreme, etwas gegen Kopfschmerzen hat auch noch nie geschadet)</li>
-                    </ul><br />
+                        <li>eine kleine Reiseapotheke (z.B. Sonnencreme, etwas gegen Kopfschmerzen
+                            hat auch noch nie geschadet)
+                        </li>
+                    </ul>
+                    <br/>
                     <span>Wer hat auch gerne:</span>
                     <ul>
                         <li>GoPro</li>
                         <li>Utensilien für Beerpong</li>
                         <li>Bälle jeglicher Art (Fußball, Football, etc.)</li>
                     </ul>
-                </li>}
+                </li>
+                {nextHint < Date.now() &&
+                <li>
+                    <span>Fakten, Fakten, Fakten:</span>
+                    <ul>
+                        <li>11,3 Mio.</li>
+                        <li>30.688</li>
+                        <li>Immaterielles Weltkulturerbe</li>
+                        <li>47 Glocken</li>
+                    </ul>
+                </li>
+                }
             </ol>
         </>
     );
