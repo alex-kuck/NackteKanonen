d80c6206eb18ea3f331423fc5c9053f8dd75c49b
Alex Kucksdorf <alexander.kucksdorf@gmail.com>
2020-01-01 23:08:02 +0100

    Add penalty for losing pin

--- src/components/penalties.tsx
+++ src/components/penalties.tsx
@@ -13,15 +13,16 @@ export const Penalties: React.FC = () => {
         { offense: 'Kugel gebracht bekommen', penalty: formattedCurrency(.5) },
         {
             offense: 'Offensichtlich warnen, dass jemand dran ist',
-            penalty: `${ formattedCurrency(0.5) } für Warnenden und Werfer`
+            penalty: `${formattedCurrency(0.5)} für Warnenden und Werfer`
         },
         { offense: 'Fehlwurf', penalty: formattedCurrency(1) },
         {
             offense: 'Erfolgreich Kugel erlaufen',
-            penalty: `${ formattedCurrency(1) } für Werfer & Wurf wird schlechtestmöglich gewertet`
+            penalty: `${formattedCurrency(1)} für Werfer & Wurf wird schlechtestmöglich gewertet`
         },
-        { offense: 'Gescheiterter Lauf', penalty: `${ formattedCurrency(1) } für den Läufer` },
+        { offense: 'Gescheiterter Lauf', penalty: `${formattedCurrency(1)} für den Läufer` },
         { offense: 'Fernbleiben ohne Entschuldigung', penalty: formattedCurrency(15) },
+        { offense: 'Verlust des Kegelpins', penalty: formattedCurrency(10) },
     ];
 
     return (
@@ -29,26 +30,26 @@ export const Penalties: React.FC = () => {
             <h2>Strafen</h2>
             <table>
                 <thead>
-                <tr>
-                    <th>Vergehen</th>
-                    <th>Strafe</th>
-                </tr>
+                    <tr>
+                        <th>Vergehen</th>
+                        <th>Strafe</th>
+                    </tr>
                 </thead>
                 <tbody>
-                { penalties.map(({ offense, penalty }, index) =>
-                    <tr key={ `penalties-${ index }` }>
-                        <td>{ offense }</td>
-                        <td>{ penalty }</td>
-                    </tr>
-                ) }
-                <tr>
-                    <td>Zu spät ohne Benachrichtigung</td>
-                    <td>
-                        { formattedCurrency(5) } bis 15 Minuten<br/>
-                        { formattedCurrency(10) } bis 30 Minuten<br/>
-                        { formattedCurrency(15) } danach
+                    {penalties.map(({ offense, penalty }, index) =>
+                        <tr key={`penalties-${index}`}>
+                            <td>{offense}</td>
+                            <td>{penalty}</td>
+                        </tr>
+                    )}
+                    <tr>
+                        <td>Zu spät ohne Benachrichtigung</td>
+                        <td>
+                            {formattedCurrency(5)} bis 15 Minuten<br />
+                            {formattedCurrency(10)} bis 30 Minuten<br />
+                            {formattedCurrency(15)} danach
                     </td>
-                </tr>
+                    </tr>
                 </tbody>
             </table>
         </>
