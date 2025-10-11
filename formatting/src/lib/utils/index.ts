2adefe6b107966536c764572fe3da2d1852b9385
Alex Kucksdorf <alexander.kucksdorf@gmail.com>
2025-10-11 12:18:13 +0200

    chore: Cleanup formatting (#39)

--- formatting/src/lib/utils/index.ts
+++ formatting/src/lib/utils/index.ts
@@ -1,17 +1,23 @@
-export const formattedNumber = (value: number) => new Intl.NumberFormat('de-DE', {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-}).format(value);
+export const formattedNumber = (value: number) =>
+    new Intl.NumberFormat('de-DE', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    }).format(value);
 
-export const formattedCurrency = (value: number) => new Intl.NumberFormat('de-DE', {
-    style: 'currency',
-    currency: 'EUR',
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-}).format(value);
+export const formattedCurrency = (value: number) =>
+    new Intl.NumberFormat('de-DE', {
+        style: 'currency',
+        currency: 'EUR',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    }).format(value);
 
-export const formattedDateFromSeconds = (seconds: number) => seconds <= 0 ? 'N/A' : formattedDate(new Date(seconds * 1000));
+export const formattedDateFromSeconds = (seconds: number) =>
+    seconds <= 0 ? 'N/A' : formattedDate(new Date(seconds * 1000));
 
-export const formattedDate = (date?: Date) => date?.toLocaleDateString('de-DE') ?? 'N/A';
+export const formattedDate = (date?: Date) =>
+    date?.toLocaleDateString('de-DE', { day: '2-digit', month: '2-digit', year: 'numeric' }) ??
+    'N/A';
 
-export const withLeadingZeros = (value: number, targetLength: number) => value.toString().padStart(targetLength, '0');
+export const withLeadingZeros = (value: number, targetLength: number) =>
+    value.toString().padStart(targetLength, '0');
